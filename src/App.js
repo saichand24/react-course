07578454ab21f92dc1193298926edf546fbcd8e3
saichand24 <kaluvalasaichand@gmail.com>
2023-06-28 16:47:41 +0530

    Building & Re-using Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
+import Todo from './components/Todo';
 function App() {
-  return <div>
-    <h1>My Todos</h1>
-    <div className="card">
+  return (
+    <div>
       <h2>Title</h2>
-      <div className="actions">
-        <button className="btn">Delete</button>
-      </div>
+      <Todo />
     </div>
-  </div>;
+  );
 }
 
 export default App;
