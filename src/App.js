8e9720fb0f5e6eda24523ee6db75a746949fcf47
saichand24 <kaluvalasaichand@gmail.com>
2023-07-09 22:48:01 +0530

    Class Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Greet } from './components/Greet';
+import Greet from './components/Greet';
+import { render } from 'react-dom';
+import Welcome from './components/Welcome';
 
-function App() {
-  return (
+class App extends Component {
+  render() {
+  return(
     <div className="App">
       <Greet />
+      <Welcome />
     </div>
   );
+}
 }
 
 export default App;
