7667530101f969592024dc21f02387b0fb53484b
saichand24 <kaluvalasaichand@gmail.com>
2023-07-10 11:40:42 +0530

    JSX

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import Greet from './components/Greet';
 import { render } from 'react-dom';
 import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
 class App extends Component {
   render() {
   return(
     <div className="App">
-      <Greet />
-      <Welcome />
+      {/* <Greet />
+      <Welcome /> */}
+      <Hello />
     </div>
   );
 }
